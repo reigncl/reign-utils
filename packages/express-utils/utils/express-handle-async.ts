bf76dd093c43fc2e0402120e84ae3a08929f8b2f
Jon Dotsoy <hi@jon.soy>
2020-01-27 18:54:21 -0300

    added dialog deprecated

--- packages/express-utils/utils/express-handle-async.ts
+++ packages/express-utils/utils/express-handle-async.ts
@@ -1,1 +1,7 @@
+import { deprecate } from 'util';
 export { errorRequestHandlerAsync, requestHandlerAsync, requestParamHandlerAsync } from '../handle-async';
+
+deprecate(
+  () => { },
+  'Module deprecated. Please use `@reignmodule/express-utils/handle-async` not `@reignmodule/express-utils/utils/express-handle-async`',
+)();
--- packages/express-utils/utils/listening-listener.ts
+++ packages/express-utils/utils/listening-listener.ts
@@ -1,1 +1,7 @@
+import { deprecate } from 'util';
 export { listeningListener } from '../listening-listener';
+
+deprecate(
+  () => { },
+  'Module deprecated. Please use `@reignmodule/express-utils/listening-listener` not `@reignmodule/express-utils/utils/listening-listener`',
+)();
