23f95f2f0ea77ab8e1ca43d2f75272ba2952e6f2
Jonathan Delgado <hi@jon.soy>
2020-12-10 13:33:51 -0300

    feat(utilpkg): fix read packagejson file

--- packages/util-pkg/README.md
+++ packages/util-pkg/README.md
@@ -1,4 +1,23 @@
+# @reignmodule/util-pkg
+
 ![NPM Publish package util-pkg](https://github.com/reigncl/reign-utils/workflows/NPM%20Publish%20package%20util-pkg/badge.svg)
 
 Read the package.json file
 
+## How to use
+
+Install with [npm](https://www.npmjs.com/).
+
+```sh
+$ npm i @reignmodule/util-pkg
+```
+
+Import `@reignmodule/util-pkg` in your code.
+
+```ts
+import * as pkg from '@reignmodule/util-pkg'
+
+console.log(`Package Name: ${pkg.pkgName}`) // Package Name: @reignmodule/util-pkg
+console.log(`Package Version: ${pkg.pkgVersion}`) // Package Version: 1.0.0
+console.log(`${pkg.pkgName}@${pkg.pkgVersion}`) // @reignmodule/util-pkg@1.0.0
+```
--- packages/util-pkg/index.ts
+++ packages/util-pkg/index.ts
@@ -12,12 +12,14 @@ const readPkg = () => {
     const pathPkg = path.resolve(`${p}/../package.json`);
     if (fs.existsSync(pathPkg)) {
       try {
-        return JSON.parse(fs.readFileSync(pathPkg, "utf8"));
+        return JSON.parse(fs.readFileSync(pathPkg, "utf8")) as {
+          [k: string]: any;
+        };
       } catch {}
     }
   }
 };
 
 const pkg = once(readPkg);
-export const pkgName = process.env.npm_package_name ?? pkg().name;
-export const pkgVersion = process.env.npm_package_version ?? pkg().version;
+export const pkgName = process.env.npm_package_name ?? pkg()?.name;
+export const pkgVersion = process.env.npm_package_version ?? pkg()?.version;
