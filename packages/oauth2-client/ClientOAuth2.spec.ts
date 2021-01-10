dc71f907d8734d9b6a642812beedfad5d693ada6
Jonathan Delgado <hi@jon.soy>
2021-01-10 02:17:43 -0300

    Publish @reignmodule/oauth2-client@0.2.0-beta.4

--- packages/oauth2-client/ClientOAuth2.spec.ts
+++ packages/oauth2-client/ClientOAuth2.spec.ts
@@ -150,18 +150,4 @@ describe('ClientOAuth2', () => {
 
     expect(await clientOAuth2.getToken()).toEqual('custonaccesstoken');
   })
-
-  it('event', () => {
-    const clientOAuth2 = new ClientOAuth2({
-      authorizationUri: `${authServer.baseurl()}/authorize`,
-      tokenUri: `${authServer.baseurl()}/token`,
-      state: 'aaaa',
-      clientId: 'asd',
-      storage: {},
-    });
-
-    clientOAuth2.once('changeCurrentUser', (asd) => {
-
-    })
-  })
 });
