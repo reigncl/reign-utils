23a7c7edd924a0c4d09960aa900400c793a5174e
Jonathan Delgado <hi@jon.soy>
2022-04-05 11:04:38 -0400

    feat(mechanics): update mechanics 4

--- packages/mechanic/MechanicsIdSupported.ts
+++ packages/mechanic/MechanicsIdSupported.ts
@@ -13,7 +13,7 @@ export type MechanicsId = typeof MechanicsIdSupported[number];
 
 export interface MechanicsMapExpression {
     "1": `${number}`
-    "4": `${number}*${number}`
+    "4": `${number}` | `${number}*${number}`
     "13": string
     "11": `${number}*${number}`
     "2": `${number}*${number}`
